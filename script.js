a8db1484143b218b07f2646c7013ae9188660a01
Izalhadji31 <ryzalhadji@gmail.com>
2024-04-09 06:03:47 +0700

    Forceku

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("video").addEventListener("click", function() {
+    alert("Ini Video CNM");
+});
+
+document.getElementById("audio").addEventListener("click", function() {
+    alert("Ini Audio CNM");
+});
+
+document.getElementById("gambar").addEventListener("click", function() {
+    alert("Ini Motorku");
+});
